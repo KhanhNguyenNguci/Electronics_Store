ea2383bdbfe4474c8c948c762d33da2f2cbe0afb
KhanhNguyenNguci <khanhnguyennlk4198@gmail.com>
2023-11-24 17:54:38 +0700

    thong ke 1st

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,37 @@
-﻿<?php include 'inc/header.php';?>
+﻿<?php
+  require('../carbon/autoload.php'); //lay ra thu, ngay, thang
+  use Carbon\Carbon;
+  use Carbon\CarbonInterval;
+?>
+<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
         <div class="grid_10">
             <div class="box round first grid">
-                <h2> Dashbord</h2>
+                <h2> Thống kê đơn hàng : <?php echo Carbon::yesterday('Asia/Ho_Chi_Minh');?> </h2>
                 <div class="block">               
-                  Welcome admin panel       
+                  <div class="col-md-3">
+                    Từ ngày: <input class="form-control date_from" type="text" id="datepicker_from"></p>
+                  </div>
+                  <div class="col-md-3">
+                    Tới ngày: <input class="form-control date_to" type="text" id="datepicker_to"></p>
+                    <input type="button" value="Lọc theo ngày" class="btn btn-success btn-locngay">     
+                  </div>
+                  <div class="col-md-3">
+                    Lọc theo: <span id="text-date"></span><br>
+                    <select class="form_control select-thongke">
+                      <option>--Lọc theo--</option>
+                      <option value="7ngay">--Lọc theo 7 ngày--</option>
+                      <option value="30ngay">--Lọc theo 30 ngày--</option>
+                      <option value="90ngay">--Lọc theo 90 ngày--</option>
+                      <option value="365ngay">--Lọc theo 1 năm--</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div id="myfisrtchart" style="height: 250px;"></div>
+                  </div>
+
                 </div>
             </div>
         </div>
---
+++ ajax/thongke.php
@@ -0,0 +1,55 @@
+<?php
+    $filepath = realpath(dirname(__FILE__));
+    include_once($filepath.'/../lib/database.php');
+    //include_once($filepath.'/../helpers/format.php');
+    require_once('../carbon/autoload.php'); //sửa dụng carbon lấy ra thứ ngày tháng
+    use Carbon\Carbon;
+    use Carbon\CarbonInterval;
+?>
+<?php
+    $db = new Database();
+    
+    if(isset($_POST['thoigian'])){
+        $thoigian = $_POST['thoigian']; //lấy ra thời gian
+    }else{
+        $thoigian = '';
+        $subdays = Carbon::now('Asia/Ho_Chi_Minh')->subdays(365)->toDateString();
+    }
+
+    if($thoigian == '7ngay'){
+        $subdays = Carbon::now('Asia/Ho_Chi_Minh')->subdays(7)->toDateString();
+    }elseif($thoigian == '30ngay'){
+        $subdays = Carbon::now('Asia/Ho_Chi_Minh')->subdays(30)->toDateString();
+    }elseif($thoigian == '90ngay'){
+        $subdays = Carbon::now('Asia/Ho_Chi_Minh')->subdays(90)->toDateString();
+    }elseif($thoigian == '365ngay'){
+        $subdays = Carbon::now('Asia/Ho_Chi_Minh')->subdays(365)->toDateString();
+    }
+
+    $now = Carbon::now('Asia/Ho_Chi_Minh')->toDateString(); //lấy time hiện tại, chỉ lấy ngày
+    if(isset($_POST['from_date']) && $_POST['from_to']){
+        $from = $_POST['from_date'];
+        $to = $_POST['from_to'];
+        $query = "SELECT * FROM tbl_thongke WHERE date_thongke BETWEEN '$from' AND '$to' ORDER BY date_thongke ASC";
+    }else{
+        $query = "SELECT * FROM tbl_thongke WHERE date_thongke BETWEEN '$subdays' AND '$now' ORDER BY date_thongke ASC";
+    }
+    $result = $db->select($query);
+    
+    //echo $query;
+    //dữ liệu là mảng gồm các phần tử phía dưới
+    $chart_data = array();
+    if($result){
+        while ($row = pg_fetch_assoc($result)) {
+            $chart_data[] = array(
+                'date' => $row['date_thongke'],
+                'order' => $row['donhang'],
+                'revenue' => $row['doanhthu'],
+                'quantity' => $row['soluong']
+            );
+        }
+    }
+
+    //print_r($chart_data);
+    echo $data = json_encode($chart_data); //dữ liệu dạng JSON
+?>
